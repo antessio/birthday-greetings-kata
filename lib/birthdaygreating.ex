2fe1113ad52fcf86d2386276b8f16c2b83d83a8a
Antonio D'Alessio <antonio.dalessio@prima.it>
2024-12-20 17:47:06 +0100

    wip

--- lib/birthdaygreating.ex
+++ lib/birthdaygreating.ex
@@ -1,18 +1,53 @@
 defmodule Birthdaygreating do
   @moduledoc """
-  Documentation for `Birthdaygreating`.
+  As you’re a very friendly person, you would like to send a birthday note to all the friends you have. But you have a lot of friends and a bit lazy, it may take some times to write all the notes by hand.
+
+  The good news is that computers can do it automatically for you.
+
+  Imagine you have a flat file with all your friends :
+
+  last_name, first_name, date_of_birth, email
+  Doe, John, 1982/10/08, john.doe@foobar.com
+  Ann, Mary, 1975/09/11, mary.ann@foobar.com
+  And you want to send them a happy birthday email on their birth date :
+
+  Subject: Happy birthday!
+
+  Happy birthday, dear <first_name>!
+  How would this software look like ? Try to implement it so you can easily change :
+
+  the way you retrieve the friends data (for instance, try switching to a SQLite Db)
+  the way you send the note : (for instance, imagine you want to send SMS instead of emails)
+  What kind of tests would you write ? Would you use Mocks ?
+
+
   """
 
-  @doc """
-  Hello world.
 
-  ## Examples
+  defmodule Message do
 
-      iex> Birthdaygreating.hello()
-      :world
+    @type t() :: %__MODULE__{
+      subject: String.t(),
+      body: String.t(),
+      recipient: String.t()
+    }
 
-  """
-  def hello do
-    :world
+    defstruct [:subject, :body, :recipient]
+  end
+
+  defmodule Friend do
+    @type t() :: %__MODULE__{
+      name: String.t()
+    }
+
+    defstruct [:name]
+
+  end
+
+  @type friend_fetcher :: (Date.t() -> [Friend.t()])
+
+  @spec send(Date.t, friend_fetcher()) :: [Message.t()]
+  def send(_date, _friend_fetcher) do
+    []
   end
 end
--- test/birthdaygreating_test.exs
+++ test/birthdaygreating_test.exs
@@ -2,7 +2,27 @@ defmodule BirthdaygreatingTest do
   use ExUnit.Case
   doctest Birthdaygreating
 
-  test "greets the world" do
-    assert Birthdaygreating.hello() == :world
+  test "No friends at all" do
+    # given a date
+    date = ~D"2024-12-20"
+
+    # when theere's no friends
+    messages = Birthdaygreating.send(date, fn _ -> [] end)
+
+    # then no message is sent
+    assert length(messages) == 0
+
+  end
+
+  test "No friends born today" do
+    # given a date
+    date = ~D"2024-12-20"
+
+    # when it's no friend's birthday
+    messages = Birthdaygreating.send(date, fn _ -> [] end)
+
+    # then no friend was sent the birthday message
+    assert length(messages) == 0
+
   end
 end
